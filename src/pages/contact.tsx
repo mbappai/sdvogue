5e094982ebdbfdda80cb75c574d3e45293147580
mujeex <mujahid.bappai@yahoo.com>
2022-12-26 09:46:36 +0100

    feat: add google directions to store

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -16,7 +16,7 @@ const Contact: React.FC = () =>{
             </li>
             <li>
                 <span>Location:</span>
-                <span>No 48, Hadeja Road, Kano State</span>    
+                <span><a href="https://www.google.com/maps/place/SD+VOGUE+NIGERIA+LTD/@12.0073198,8.5557978,16.38z/data=!4m5!3m4!1s0x11ae81a9c43880ef:0x7097d1a983613cf3!8m2!3d12.0074995!4d8.5570063">No 48, Hadeja Road, Kano State</a></span>    
             </li>
             <li>
                 <span>Email:</span>
